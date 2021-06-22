04a740d3e836aa2fc6fd2a5ea43932646e3c15e8
Milind Mishra <milind.mishra4@gmail.com>
2021-06-23 00:56:21 +0530

    cpp template file

---
+++ cpp-template.cpp
@@ -0,0 +1,40 @@
+// Problem Link -
+/* By Milind Mishra */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll              long long int
+#define ld              long double
+#define mod             1000000007
+#define inf             1e18
+#define endl            "\n"
+#define pb              __emplace_back
+#define vi              vector<ll>
+#define vs              vector<string>
+#define pii             pair<ll,ll>
+#define ump             unordered_map
+#define mp              map
+#define pq_max          priority_queue<ll>
+#define pq_min          priority_queue<ll,vi,greater<ll> >
+#define ff              first
+#define ss              second
+#define mid(l,r)        (l+(r-l)/2)
+#define loop(i,a,b)     for(int i=(a);i<=(b);i++)
+#define looprev(i,a,b)  for(int i=(a);i>=(b);i--)
+#define log(args...)    { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) cout<<(arr[z])<<" ";cout<<endl;
+
+void file_i_o()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(int argc, char const *argv[]) {
+	file_i_o();
+	return 0;
+}
